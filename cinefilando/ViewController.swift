8b3ecda3d4411d52c17aab000dfeed9ae25940e0
lucasrsv <lucasrsvalenca@gmail.com>
2022-07-06 12:56:46 -0300

    Initial Commit

---
+++ cinefilando/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cinefilando
+//
+//  Created by lrsv on 06/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
